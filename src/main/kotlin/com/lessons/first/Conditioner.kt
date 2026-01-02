34ab9f30704a3db52a66cbe66a1776e27c9b13ac
kerikir <vlasov-kerik@mail.ru>
2026-01-02 22:00:57 +0300

    added solution the conditioner task

--- src/main/kotlin/com/lessons/first/Conditioner.kt
+++ src/main/kotlin/com/lessons/first/Conditioner.kt
@@ -1,2 +1,16 @@
 package com.lessons.first
 
+fun main() {
+    val (troom, tcond) = readln().split(" ").map { it.toInt() }
+    val mode = readln().trim()
+
+    var result = troom
+
+    when {
+        (mode == "freeze") && (tcond < troom) -> result = tcond
+        mode.equals("heat") && (tcond > troom) -> result = tcond
+        mode == "auto" -> result = tcond
+    }
+
+    println(result)
+}
