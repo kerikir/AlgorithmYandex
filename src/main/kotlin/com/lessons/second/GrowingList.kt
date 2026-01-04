492194c32b6466d0b59bc7d0932b52a8bddcc04d
kerikir <vlasov-kerik@mail.ru>
2026-01-05 00:51:07 +0300

    added an ascending list check

---
+++ src/main/kotlin/com/lessons/second/GrowingList.kt
@@ -0,0 +1,27 @@
+package com.lessons.second
+
+
+/*
+Время = 173ms
+Память = 14.57Mb
+ */
+fun main() {
+    val list = readLine()!!.trim().split(" ").map { it.toInt() }
+
+    val result = if (isGrowingList(list)) "YES" else "NO"
+    println(result)
+}
+
+
+fun isGrowingList(list: List<Int>) : Boolean {
+
+    var flag = true
+
+    for (i in 1..list.size - 1) {
+        if (list[i] <= list[i - 1]) {
+            flag = false
+        }
+    }
+
+    return flag
+}
