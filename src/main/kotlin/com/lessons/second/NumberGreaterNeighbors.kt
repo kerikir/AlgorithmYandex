c05de58c0d952364d9a3842f09ee441d8c51e7af
kerikir <vlasov-kerik@mail.ru>
2026-01-05 02:05:16 +0300

    added a definition of the number of numbers greater than the neighbors

---
+++ src/main/kotlin/com/lessons/second/NumberGreaterNeighbors.kt
@@ -0,0 +1,28 @@
+package com.lessons.second
+
+
+/*
+Время = 173ms
+Память = 14.70Mb
+ */
+fun main() {
+    val array = readLine()!!.trim().split(" ").map { it.toInt() }
+
+    val result = counterNumbersHigherThanNeighbors(array)
+    println(result)
+}
+
+
+fun counterNumbersHigherThanNeighbors(list: List<Int>) : Int {
+    var counter = 0
+
+    if (list.isNullOrEmpty() || list.size <= 2) return 0
+
+    for (i in 1..list.lastIndex - 1) {
+        if (list[i - 1] < list[i] && list[i] > list[i + 1]) {
+            counter++
+        }
+    }
+
+    return counter
+}
