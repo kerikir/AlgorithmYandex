a06b72fa02194ce923bf7398836e861bd192e2c9
kerikir <vlasov-kerik@mail.ru>
2026-01-05 01:57:02 +0300

    added the definition of the nearest number

---
+++ src/main/kotlin/com/lessons/second/NearestNumber.kt
@@ -0,0 +1,36 @@
+package com.lessons.second
+
+import kotlin.math.abs
+
+
+/*
+Время = 173ms
+Память = 14.82Mb
+ */
+fun main() {
+    val size = readLine()!!.trim().toShort()
+    val numbers = readLine()!!.trim().split(" ").map { it.toShort() }
+    val number = readLine()!!.trim().toShort()
+
+    val result = definitionNearestNumber(numbers, number)
+    println(result)
+}
+
+
+fun definitionNearestNumber(list: List<Short>, number: Short) : Short {
+
+    var diff = abs(number - list[0]).toShort()
+    var index = 0
+    var currentDiff: Short
+
+    for (i in list.indices) {
+        currentDiff = abs(number - list[i]).toShort()
+
+        if (currentDiff < diff) {
+            diff = currentDiff
+            index = i
+        }
+    }
+
+    return list[index]
+}
