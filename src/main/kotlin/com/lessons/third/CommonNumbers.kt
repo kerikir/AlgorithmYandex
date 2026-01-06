3c108ad13e58a0e0864f6c81d4ec6771b3fc7fe6
kerikir <vlasov-kerik@mail.ru>
2026-01-06 21:24:31 +0300

    added definition of common numbers

---
+++ src/main/kotlin/com/lessons/third/CommonNumbers.kt
@@ -0,0 +1,22 @@
+package com.lessons.third
+
+
+/*
+Время = 0.698s
+Память = 38.41Mb
+ */
+fun main() {
+    val numbers1 = readLine()!!.trim().split(" ").map { it.toInt() }
+    val numbers2 = readLine()!!.trim().split(" ").map { it.toInt() }
+
+    val result = definingCommonNumbers(numbers1, numbers2)
+    println(result.joinToString(" "))
+}
+
+
+fun definingCommonNumbers(list1: List<Int>, list2: List<Int>) : List<Int> {
+    val set1 = list1.toSet()
+    val set2 = list2.toSet()
+    val setIntersection = list1.intersect(set2)
+    return setIntersection.sorted()
+}
