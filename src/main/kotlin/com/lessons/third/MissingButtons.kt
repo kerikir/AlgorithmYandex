645bf54a119b1e689e3b13ee16d042be01a3a5af
kerikir <vlasov-kerik@mail.ru>
2026-01-07 01:01:35 +0300

    added a search for missing buttons

---
+++ src/main/kotlin/com/lessons/third/MissingButtons.kt
@@ -0,0 +1,22 @@
+package com.lessons.third
+
+
+fun main() {
+    val buttons = readLine()!!.trim().split(" ").map { it.toInt() }
+    val n = readLine()!!.trim().toInt()
+
+    val result = determinateMissingButtons(buttons, n)
+    println(result)
+}
+
+
+fun determinateMissingButtons(list: List<Int>, number: Int) : Int {
+
+    // Определение из каких букв состоит нужное число
+    val digits = number.toString().map { it.toString().toInt() }.toSet()
+    val set = list.toSet()
+
+    // Определение нехватающих цифр
+    val missingDigits = digits.subtract(set)
+    return missingDigits.size
+}
