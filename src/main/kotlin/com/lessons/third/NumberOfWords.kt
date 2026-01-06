c34b34e634e2f36ff2782a2c05e744468561e906
kerikir <vlasov-kerik@mail.ru>
2026-01-06 22:54:55 +0300

    added a word search in the text

---
+++ src/main/kotlin/com/lessons/third/NumberOfWords.kt
@@ -0,0 +1,20 @@
+package com.lessons.third
+
+import java.io.File
+
+fun main() {
+    val text = File("input.txt").readText()
+
+    val result = determinateNumbersOfWords(text)
+    File("output.txt").writeText(result.toString())
+}
+
+
+fun determinateNumbersOfWords(text: String) : Int {
+    // Удаление знаков пунктуации
+    val cleanText = text.replace("[,.;:!?]".toRegex(), "")
+    val textOneLine = cleanText.replace("[\\s+]".toRegex(), " ").replace("\\s+".toRegex(), " ")
+
+    val words = textOneLine.toLowerCase().split(" ").toSet()
+    return words.size
+}
