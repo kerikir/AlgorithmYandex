ef2a49ec129f1c5d79579da59b13977317f890a9
kerikir <vlasov-kerik@mail.ru>
2026-01-07 23:29:16 +0300

    added a synonym search in the dictionary

---
+++ src/main/kotlin/com/lessons/fourth/DictionaryOfSynonyms.kt
@@ -0,0 +1,33 @@
+package com.lessons.fourth
+
+
+fun main() {
+
+    val n = readLine()!!.trim().toInt()
+    val words = mutableListOf<Pair<String, String>>()
+    for (i in 1..n) {
+        val (a, b) = readLine()!!.trim().split(" ")
+        words.add(a to b)
+    }
+    val word = readLine()!!.trim()
+
+    val result = determinateSynonym(words, word)
+    println(result)
+}
+
+
+
+fun determinateSynonym(words: List<Pair<String, String>>, word: String) : String {
+    val synonyms = mutableMapOf<String, String>()
+    words.forEach {
+        synonyms.put(it.first, it.second)
+    }
+
+    var synonym = synonyms[word]
+
+    if (synonym.isNullOrEmpty()) {
+        synonym = synonyms.entries.find { it.value.equals(word) }?.key
+    }
+
+    return synonym ?: ""
+}
