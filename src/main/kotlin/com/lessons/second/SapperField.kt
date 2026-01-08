7a0656b94bdaaaff5132091078de310f0061bbda
kerikir <vlasov-kerik@mail.ru>
2026-01-08 23:29:18 +0300

    added the output of the field for the minesweeper

---
+++ src/main/kotlin/com/lessons/second/SapperField.kt
@@ -0,0 +1,45 @@
+package com.lessons.second
+
+
+fun main() {
+
+    val (n, m, k)  = readLine()!!.trim().split(" ").map { it.toInt() }
+    val coords = mutableListOf<Pair<Int, Int>>()
+    for (i in 1..k) {
+        val (p, q) = readLine()!!.trim().split(" ").map { it.toInt() }
+        coords.add(p to q)
+    }
+
+    val result = fillFieldWithMines(n, m, coords)
+    println(result)
+}
+
+
+
+fun fillFieldWithMines(rows: Int, columns: Int, coords: List<Pair<Int, Int>>) : String {
+
+    val field = Array(rows) { IntArray(columns) { 0 } }
+
+    for (mine in coords) {
+        for (i in -1..1) {
+            for (j in -1..1) {
+                if (
+                    (mine.first + i <= rows) && (mine.second + j <= columns) &&
+                    (mine.first + i >= 1) && (mine.second + j >= 1)
+                ) {
+                    field[mine.first + i - 1][mine.second + j - 1]++
+                }
+            }
+        }
+    }
+
+    for (mine in coords) {
+        field[mine.first - 1][mine.second - 1] = -1
+    }
+
+    var result = ""
+    for (row in field) {
+        result += row.joinToString(" ", postfix = "\n")
+    }
+    return result
+}
