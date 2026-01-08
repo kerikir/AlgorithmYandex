345c8ec3dd27efed2f7ce6f646b57cf7338a6306
kerikir <vlasov-kerik@mail.ru>
2026-01-09 00:13:18 +0300

    added the comments in the SoundFrequency.kt

--- src/main/kotlin/com/lessons/second/SoundFrequency.kt
+++ src/main/kotlin/com/lessons/second/SoundFrequency.kt
@@ -6,6 +6,11 @@ import kotlin.math.min
 import kotlin.math.pow
 
 
+
+/*
+Время = 266ms
+Память = 18.95Mb
+ */
 fun main() {
     val n = readLine()!!.trim().toInt()
 
@@ -23,6 +28,7 @@ fun main() {
 }
 
 
+
 fun definitionSoundFrequency(measurements: List<Pair<Float, String>>) : String {
 
     var leftBorder = 30.0f
@@ -33,12 +39,15 @@ fun definitionSoundFrequency(measurements: List<Pair<Float, String>>) : String {
     for (i in 1..measurements.lastIndex) {
 
         if (measurements[i].first - measurements[i - 1].first < eps) {
+
             if (measurements[i].second == "closer") {
                 rightBorder = min(rightBorder, abs(measurements[i].first + measurements[i - 1].first) / 2.0f)
             } else {
                 leftBorder = max(leftBorder, abs(measurements[i].first + measurements[i - 1].first) / 2.0f)
             }
+
         } else if (measurements[i].first - measurements[i - 1].first > eps) {
+
             if (measurements[i].second == "closer") {
                 leftBorder = max(leftBorder, abs(measurements[i].first + measurements[i - 1].first) / 2.0f)
             } else {
