0a6c11456d04c81e316196de4e03de5a086aa0aa
kerikir <vlasov-kerik@mail.ru>
2026-01-09 00:11:33 +0300

    added sound frequency search

---
+++ src/main/kotlin/com/lessons/second/SoundFrequency.kt
@@ -0,0 +1,51 @@
+package com.lessons.second
+
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.pow
+
+
+fun main() {
+    val n = readLine()!!.trim().toInt()
+
+    val measurements = mutableListOf<Pair<Float, String>>()
+    val freq = readLine()!!.trim().toFloat()
+
+    measurements.add(freq to "")
+    for (i in 2..n) {
+        val (freq, ans) = readLine()!!.trim().split(" ")
+        measurements.add(freq.toFloat() to ans)
+    }
+
+    val result = definitionSoundFrequency(measurements)
+    println(result)
+}
+
+
+fun definitionSoundFrequency(measurements: List<Pair<Float, String>>) : String {
+
+    var leftBorder = 30.0f
+    var rightBorder = 4000.0f
+
+    val eps = 10.0.pow(-5)
+
+    for (i in 1..measurements.lastIndex) {
+
+        if (measurements[i].first - measurements[i - 1].first < eps) {
+            if (measurements[i].second == "closer") {
+                rightBorder = min(rightBorder, abs(measurements[i].first + measurements[i - 1].first) / 2.0f)
+            } else {
+                leftBorder = max(leftBorder, abs(measurements[i].first + measurements[i - 1].first) / 2.0f)
+            }
+        } else if (measurements[i].first - measurements[i - 1].first > eps) {
+            if (measurements[i].second == "closer") {
+                leftBorder = max(leftBorder, abs(measurements[i].first + measurements[i - 1].first) / 2.0f)
+            } else {
+                rightBorder = min(rightBorder, abs(measurements[i].first + measurements[i - 1].first) / 2.0f)
+            }
+        }
+    }
+
+    return "$leftBorder $rightBorder"
+}
