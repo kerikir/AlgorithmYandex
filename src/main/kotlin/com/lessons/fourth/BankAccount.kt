054f78b2d680c486b741e2c4bec1c3ec77adafa9
kerikir <vlasov-kerik@mail.ru>
2026-01-08 20:07:02 +0300

    added the output of financial transaction results

--- src/main/kotlin/com/lessons/fourth/BankAccount.kt
+++ src/main/kotlin/com/lessons/fourth/BankAccount.kt
@@ -3,7 +3,10 @@ package com.lessons.fourth
 import java.io.File
 
 
-
+/*
+Время = 0.535s
+Память = 42.45Mb
+*/
 fun main() {
     val text = File("input.txt").readText().trim().split("\r\n")
 
@@ -25,9 +28,16 @@ fun simulateBankingOperations(operations: List<List<String>>) : String {
 
     for (operation in operations) {
 
-
+        when (operation[0]) {
+            "DEPOSIT" -> depositOperation(accounts, operation)
+            "INCOME" -> incomeOperation(accounts, operation)
+            "BALANCE" -> output.add(balanceOperation(accounts, operation))
+            "WITHDRAW" -> withdrawOperation(accounts, operation)
+            "TRANSFER" -> transferOperation(accounts, operation)
+        }
     }
 
+    return output.joinToString("\n")
 }
 
 
@@ -46,11 +56,19 @@ fun withdrawOperation(accounts: MutableMap<String, Int>, operation: List<String>
 
 
 
+fun transferOperation(accounts: MutableMap<String, Int>, operation: List<String>) {
+
+    accounts[operation[1]] = accounts.getOrDefault(operation[1], 0) - operation[3].toInt()
+    accounts[operation[2]] = accounts.getOrDefault(operation[2], 0) + operation[3].toInt()
+}
+
+
+
 fun incomeOperation(accounts: MutableMap<String, Int>, operation: List<String>) {
 
-    accounts.forEach { name, sum ->
+    accounts.forEach { (name, sum) ->
         if (sum > 0) {
-            accounts[name] = (sum * (100.0 + operation[1].toInt())).toInt()
+            accounts[name] = (sum * (100.0 + operation[1].toInt()) / 100.0).toInt()
         }
     }
 }
@@ -59,9 +77,9 @@ fun incomeOperation(accounts: MutableMap<String, Int>, operation: List<String>) 
 
 fun balanceOperation(accounts: MutableMap<String, Int>, operation: List<String>) : String {
 
-    if (accounts.contains(operation[1])) {
-        return accounts[operation[1]].toString()
+    return if (accounts.contains(operation[1])) {
+        accounts[operation[1]].toString()
     } else {
-        return "ERROR"
+        "ERROR"
     }
 }
