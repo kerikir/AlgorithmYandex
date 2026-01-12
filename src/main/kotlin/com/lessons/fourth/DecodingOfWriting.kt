46199651cb5181cd54848d31c8859b3d91b6e0e4
kerikir <vlasov-kerik@mail.ru>
2026-01-12 21:32:12 +0300

    added the DecodingOfWriting.kt in the project

---
+++ src/main/kotlin/com/lessons/fourth/DecodingOfWriting.kt
@@ -0,0 +1,15 @@
+package com.lessons.fourth
+
+
+fun main() {
+    val (g, s) = readLine()!!.trim().split(" ").map { it.toInt() }
+    val w = readLine()!!.trim().map { it }
+    val text = readLine()!!.trim()
+}
+
+
+fun decodeOfWriting(symbols: List<Char>, text: String) : Int {
+    val variationsOfWord = mutableMapOf<String, Int>()
+
+    return 0
+}
