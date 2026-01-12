3bee3c717669be4e23547e81bc11896ba253b46c
kerikir <vlasov-kerik@mail.ru>
2026-01-12 23:38:53 +0300

    added definition of the number of line segments for a point

---
+++ src/main/kotlin/com/lessons/seventh/SegmentsAndPoints.kt
@@ -0,0 +1,36 @@
+package com.lessons.seventh
+
+fun main() {
+    val (n, m) = readLine()!!.trim().split(" ").map { it.toInt() }
+    val segments = mutableListOf<Pair<Int, Int>>()
+    for (i in 1..n) {
+        val (a, b) = readLine()!!.trim().split(" ").map { it.toInt() }
+        segments.add(a to b)
+    }
+    val points = readLine()!!.trim().split(" ").map { it.toInt() }
+
+    val result = determinateNumberOfPointsOnSegments(points, segments)
+    println(result)
+}
+
+
+fun determinateNumberOfPointsOnSegments(points: List<Int>, segments: List<Pair<Int, Int>>) : String {
+
+    val amountSegmentsList = mutableListOf<Int>()
+
+    var amountSegments = 0
+
+    for (point in points) {
+        amountSegments = 0
+
+        for (segment in segments) {
+            if (point >= segment.first && point <= segment.second) {
+                amountSegments++
+            }
+        }
+
+        amountSegmentsList.add(amountSegments)
+    }
+
+    return amountSegmentsList.joinToString(" ")
+}
