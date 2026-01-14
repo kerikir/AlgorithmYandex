f4cded3bc03e6129b9b7a6da4c6a3c6841112e56
kerikir <vlasov-kerik@mail.ru>
2026-01-14 20:56:41 +0300

    added the comments in the Triangle.kt

--- src/main/kotlin/com/lessons/first/Triangle.kt
+++ src/main/kotlin/com/lessons/first/Triangle.kt
@@ -4,12 +4,16 @@ package com.lessons.first
 /*
 Время = 161ms
 Память = 13.79Mb
+
+Сложность = O(1)
  */
 fun main() {
+    // O(1)
     val a = readLine()!!.trim().toInt()
     val b = readLine()!!.trim().toInt()
     val c = readLine()!!.trim().toInt()
 
+    // O(1)
     val result: String = if (
         (a + b > c) &&
         (a + c > b) &&
@@ -20,5 +24,6 @@ fun main() {
         "NO"
     }
 
+    // O(1)
     print(result)
 }
