319368843236f41cdb173083cba603f4883cf28b
kerikir <vlasov-kerik@mail.ru>
2026-01-14 20:39:18 +0300

    added the comments in the Conditioner.kt

--- src/main/kotlin/com/lessons/first/Conditioner.kt
+++ src/main/kotlin/com/lessons/first/Conditioner.kt
@@ -3,18 +3,24 @@ package com.lessons.first
 /*
 Время = 171ms
 Память = 14.70Mb
+
+Сложность = O(1)
  */
 fun main() {
+
+    // O(1)
     val (troom, tcond) = readLine()!!.split(" ").map { it.toInt() }
     val mode = readln().trim()
 
     var result = troom
 
+    // O(1)
     when {
         (mode == "freeze") && (tcond < troom) -> result = tcond
         mode.equals("heat") && (tcond > troom) -> result = tcond
         mode == "auto" -> result = tcond
     }
 
+    // O(1)
     println(result)
 }
