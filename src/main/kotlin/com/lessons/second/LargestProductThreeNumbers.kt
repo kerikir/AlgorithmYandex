594a103a7c74e7c136a4d4a80de4ef81f9ab890f
kerikir <vlasov-kerik@mail.ru>
2026-01-15 15:03:59 +0300

    added the comments in the LargestProductThreeNumbers.kt

--- src/main/kotlin/com/lessons/second/LargestProductThreeNumbers.kt
+++ src/main/kotlin/com/lessons/second/LargestProductThreeNumbers.kt
@@ -4,6 +4,8 @@ package com.lessons.second
 /*
 Время = 477ms
 Память = 29.70Mb
+
+Сложность = O(N)
  */
 fun main() {
     val numbers = readLine()!!.trim().split(" ").map { it.toInt() }
@@ -23,21 +25,26 @@ fun largestMultiplicationThreeNumbers(list: List<Int>) : Triple<Int, Int, Int> {
     for (i in list.indices) {
         // Находим максимальные числа в последовательности
         if (list[i] > max1) {
+            // Первый максимум
             max3 = max2
             max2 = max1
             max1 = list[i]
         } else if (list[i] > max2) {
+            // Второй максимум
             max3 = max2
             max2 = list[i]
         } else if (list[i] > max3) {
+            // Третий максимум
             max3 = list[i]
         }
 
         // Находим минимальные числа в последовательности
         if (list[i] < min1) {
+            // Первый минимум
             min2 = min1
             min1 = list[i]
         } else if (list[i] < min2) {
+            // Второй минимум
             min2 = list[i]
         }
     }
