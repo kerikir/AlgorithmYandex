d7a71616d1182adefb2a01fa57f4aec01440b88d
kerikir <vlasov-kerik@mail.ru>
2026-01-15 15:24:24 +0300

    added the comments in the SoundFrequency.kt

--- src/main/kotlin/com/lessons/second/SoundFrequency.kt
+++ src/main/kotlin/com/lessons/second/SoundFrequency.kt
@@ -10,6 +10,8 @@ import kotlin.math.pow
 /*
 Время = 266ms
 Память = 18.95Mb
+
+Сложность = O(N)
  */
 fun main() {
     val n = readLine()!!.trim().toInt()
@@ -31,9 +33,11 @@ fun main() {
 
 fun definitionSoundFrequency(measurements: List<Pair<Float, String>>) : String {
 
+    // Границы диапазона
     var leftBorder = 30.0f
     var rightBorder = 4000.0f
 
+    // Точность измерений
     val eps = 10.0.pow(-5)
 
     for (i in 1..measurements.lastIndex) {
@@ -41,16 +45,20 @@ fun definitionSoundFrequency(measurements: List<Pair<Float, String>>) : String {
         if (measurements[i].first - measurements[i - 1].first < eps) {
 
             if (measurements[i].second == "closer") {
+                // Меньшая частота и ближе
                 rightBorder = min(rightBorder, abs(measurements[i].first + measurements[i - 1].first) / 2.0f)
             } else {
+                // Меньшая частота и дальше
                 leftBorder = max(leftBorder, abs(measurements[i].first + measurements[i - 1].first) / 2.0f)
             }
 
         } else if (measurements[i].first - measurements[i - 1].first > eps) {
 
             if (measurements[i].second == "closer") {
+                // Большая частота и ближе
                 leftBorder = max(leftBorder, abs(measurements[i].first + measurements[i - 1].first) / 2.0f)
             } else {
+                // Большая частота и дальше
                 rightBorder = min(rightBorder, abs(measurements[i].first + measurements[i - 1].first) / 2.0f)
             }
         }
