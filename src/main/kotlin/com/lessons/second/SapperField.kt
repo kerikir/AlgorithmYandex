cbc1b70884f82afd6d507fd9ddcc79cf5a859902
kerikir <vlasov-kerik@mail.ru>
2026-01-15 15:13:28 +0300

    added the comments in the SapperField.kt

--- src/main/kotlin/com/lessons/second/SapperField.kt
+++ src/main/kotlin/com/lessons/second/SapperField.kt
@@ -4,9 +4,12 @@ package com.lessons.second
 /*
 Время = 194ms
 Память = 14.95Mb
+
+Сложность = O(N*M+K) = O(N^2)
  */
 fun main() {
 
+    // O(K)
     val (n, m, k)  = readLine()!!.trim().split(" ").map { it.toInt() }
     val coords = mutableListOf<Pair<Int, Int>>()
     for (i in 1..k) {
@@ -15,6 +18,7 @@ fun main() {
     }
 
     val result = fillFieldWithMines(n, m, coords)
+    // O(N*M)
     println(result)
 }
 
@@ -22,11 +26,15 @@ fun main() {
 
 fun fillFieldWithMines(rows: Int, columns: Int, coords: List<Pair<Int, Int>>) : String {
 
+    // O(N*M)
     val field = Array(rows) { IntArray(columns) { 0 } }
 
+    // O(9*K) = O(K)
+    // Проход по всем минам
     for (mine in coords) {
         for (i in -1..1) {
             for (j in -1..1) {
+                // Проверка на выход за границы
                 if (
                     (mine.first + i <= rows) && (mine.second + j <= columns) &&
                     (mine.first + i >= 1) && (mine.second + j >= 1)
@@ -37,10 +45,13 @@ fun fillFieldWithMines(rows: Int, columns: Int, coords: List<Pair<Int, Int>>) : 
         }
     }
 
+    // Установка мин
     for (mine in coords) {
         field[mine.first - 1][mine.second - 1] = -1
     }
 
+    // O(N*M)
+    // Заполнение поля для игры
     var result = ""
     for (i in field.indices) {
         for (j in field[i].indices) {
