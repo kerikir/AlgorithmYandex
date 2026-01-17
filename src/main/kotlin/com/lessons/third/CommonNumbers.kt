aba937c5e139d2079d5ed34fc0fad35487216300
kerikir <vlasov-kerik@mail.ru>
2026-01-17 18:53:41 +0300

    added the comments in the CommonNumbers.kt

--- src/main/kotlin/com/lessons/third/CommonNumbers.kt
+++ src/main/kotlin/com/lessons/third/CommonNumbers.kt
@@ -4,6 +4,8 @@ package com.lessons.third
 /*
 Время = 0.698s
 Память = 38.41Mb
+
+Сложность = O(N + M + K * logK)
  */
 fun main() {
     val numbers1 = readLine()!!.trim().split(" ").map { it.toInt() }
@@ -15,8 +17,12 @@ fun main() {
 
 
 fun definingCommonNumbers(list1: List<Int>, list2: List<Int>) : List<Int> {
+    // O(N)
     val set1 = list1.toSet()
+    // O(M)
     val set2 = list2.toSet()
+    // O(min(N, M))
     val setIntersection = set1.intersect(set2)
+    // O(K * logK)
     return setIntersection.sorted()
 }
