441561c887d4ad61c14e6be368d67db64a1cde35
kerikir <vlasov-kerik@mail.ru>
2026-01-17 21:16:05 +0300

    added the comments in the TurtleRacing.kt

--- src/main/kotlin/com/lessons/third/TurtleRacing.kt
+++ src/main/kotlin/com/lessons/third/TurtleRacing.kt
@@ -4,6 +4,8 @@ package com.lessons.third
 /*
 Время = 331ms
 Память = 23.79Mb
+
+Сложность = O(2 * N) = O(N)
 */
 fun main() {
     val n = readLine()!!.trim().toInt()
@@ -21,11 +23,14 @@ fun main() {
 
 fun determinateTurtleRacing(turtlesPlace: List<Pair<Int, Int>>, size: Int) : Int {
 
+    // O(N)
     val possiblePlaces = mutableSetOf<Pair<Int, Int>>()
     for (i in 0..size - 1) {
         possiblePlaces.add(i to (size - 1 - i))
     }
 
+    // O(N)
     val rightPlaces = turtlesPlace.intersect(possiblePlaces)
+    // O(1)
     return rightPlaces.size
 }
