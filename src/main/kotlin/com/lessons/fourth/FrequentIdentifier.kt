4e34276a9ddd45198097463b5cf63500ee195c26
kerikir <vlasov-kerik@mail.ru>
2026-01-21 21:00:43 +0300

    added the comments in the FrequentIdentifier.kt

--- src/main/kotlin/com/lessons/fourth/FrequentIdentifier.kt
+++ src/main/kotlin/com/lessons/fourth/FrequentIdentifier.kt
@@ -8,9 +8,12 @@ import java.util.LinkedHashMap
 /*
 Время = 290ms
 Память = 20.59Mb
+
+Сложность = O(L + N * S + K)
  */
 fun main() {
-    val text = File("input.txt").readText().trim().split("\r\n")
+
+    val text = File("input.txt").readText().trim().split("\r\n")  // O(L)
 
     val (n, c, d) = text[0].split(' ')
 
@@ -23,7 +26,7 @@ fun main() {
 
     // Определение всех ключевых слов
     val keywords = mutableSetOf<String>()
-    for (i in 1..numberOfKeywords) {
+    for (i in 1..numberOfKeywords) {   // O(M * S)
 
         val keyword = text[i]
 
@@ -37,7 +40,7 @@ fun main() {
 
     // Получение кода программы
     val program = mutableListOf<String>()
-    for (i in (numberOfKeywords + 1) until text.size) {
+    for (i in (numberOfKeywords + 1) until text.size) {   // O(L)
         program.add(text[i])
     }
 
@@ -51,6 +54,7 @@ fun determinateMostFrequentIdentifier(
     program: List<String>, keywords: Set<String>, caseSensitivity: Boolean, identifierWithDigit: Boolean
 ) : String {
 
+    // O(L + N)
     // Получение всех идентификаторов и ключевых слов в программе
     val words = program.joinToString("\n")
         .replace(Regex("[^A-Za-z0-9_]"), " ")
@@ -62,34 +66,34 @@ fun determinateMostFrequentIdentifier(
         return ""
     }
 
-    val identifications = LinkedHashMap<String, Int>()
+    val identifications = LinkedHashMap<String, Int>()  // O(1)
 
     // Пробег по всем словам
-    for (word in words) {
+    for (word in words) {   // O(N)
 
         // Получение идентификатора в зависимости от специфики языка
         val identification = if (caseSensitivity) {
             word
         } else {
-            word.toLowerCase()
+            word.toLowerCase()  // O(S)
         }
 
         // Не является ключевым словом
-        if (identification !in keywords) {
+        if (identification !in keywords) {      // O(1)
 
             // Идентификатор не подходит
             if (identification.first().isDigit() && !identifierWithDigit) {
                 // Первая цифра в идентификаторе и так запрещено в языке
                 continue
             }
-            if (identification.all { it.isDigit() }) {
+            if (identification.all { it.isDigit() }) {    // O(S)
                 // Идентификатор состоит только из цифр
                 continue
             }
 
             // Подсчет частоты идентификаторов
-            val count = identifications.getOrDefault(identification, 0)
-            identifications[identification] = count + 1
+            val count = identifications.getOrDefault(identification, 0)    // O(1)
+            identifications[identification] = count + 1                               // O(1)
         }
     }
 
@@ -98,7 +102,7 @@ fun determinateMostFrequentIdentifier(
     }
 
     // Максимальная частота повторения идентификатора
-    val maxFrequency = identifications.maxOf { it.value }
+    val maxFrequency = identifications.maxOf { it.value }   // O(K)
 
-    return identifications.entries.first { it.value == maxFrequency }.key
+    return identifications.entries.first { it.value == maxFrequency }.key   // O(K)
 }
