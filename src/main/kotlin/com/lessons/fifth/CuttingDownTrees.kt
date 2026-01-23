8c14f1fbec8b45b35645f2f76cb61642762c096e
kerikir <vlasov-kerik@mail.ru>
2026-01-23 05:59:07 +0300

    the name of the identifiers has been changed

--- src/main/kotlin/com/lessons/fifth/CuttingDownTrees.kt
+++ src/main/kotlin/com/lessons/fifth/CuttingDownTrees.kt
@@ -4,31 +4,31 @@ package com.lessons.fifth
 fun main() {
 
     val (n, k) = readLine()!!.trim().split(' ').map { it.toInt() }
-    val treesVariety = readLine()!!.trim().split(' ').map { it.toInt() }
+    val treesVarieties = readLine()!!.trim().split(' ').map { it.toInt() }
 
-    val result = determinateMinLengthSegment(treesVariety, k)
+    val result = determinateMinLengthSegment(treesVarieties, k)
     println("${result.first} ${result.second}")
 }
 
 
-fun determinateMinLengthSegment(treesVariety: List<Int>, numberOfVariety: Int) : Pair<Int, Int> {
+fun determinateMinLengthSegment(treesVariety: List<Int>, numberOfVarieties: Int) : Pair<Int, Int> {
 
     // Массив флагов вхождения в диапазон
-    val flagsVariety = BooleanArray(treesVariety.size)
+    val flagsVarieties = BooleanArray(treesVariety.size)
 
     // Количество сортов на отрезке
-    var counterVariety = 0
+    var counterVarieties = 0
 
     // Определение количества сортов на отрезке
-    for (i in 0 until numberOfVariety) {
+    for (i in 0 until numberOfVarieties) {
 
         val currentVariety = treesVariety[i] - 1
 
         // Нет сорта на отрезке
-        if (!flagsVariety[currentVariety]) {
+        if (!flagsVarieties[currentVariety]) {
 
-            flagsVariety[currentVariety] = true
-            counterVariety++
+            flagsVarieties[currentVariety] = true
+            counterVarieties++
         }
     }
 
