5b623502ac0470ae82eb42041dedd496ea8fdaf0
kerikir <vlasov-kerik@mail.ru>
2026-01-23 19:38:54 +0300

    added sorting input data

--- src/main/kotlin/com/lessons/fifth/PurchaseConditioners.kt
+++ src/main/kotlin/com/lessons/fifth/PurchaseConditioners.kt
@@ -17,6 +17,9 @@ fun main() {
         val (b, c) = readLine()!!.trim().split(' ').map { it.toInt() }
         infoAboutConditioners.add(b to c)
     }
+
+    val result = calculateCostConditioners(a, infoAboutConditioners)
+    println(result)
 }
 
 
@@ -24,6 +27,12 @@ fun main() {
 fun calculateCostConditioners(
     requirementOfClasses: List<Int>, infoAboutConditioners: List<Pair<Int, Int>>
 ) : Int {
+
+    // Сортировка классов по требованиям к кондиционеру
+    val sortedClasses = requirementOfClasses.sorted()
+
+    // Сортировка кондиционеров по стоимости
+    val sortedConditioners = infoAboutConditioners.sortedBy { it.second }
 
     var sumCost = 0
 
