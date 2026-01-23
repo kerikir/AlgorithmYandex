55ddeccb171c2a43a3dcda4c9d7f2bb54a600c02
kerikir <vlasov-kerik@mail.ru>
2026-01-23 05:55:23 +0300

    added definition of the number of varieties on a segment

--- src/main/kotlin/com/lessons/fifth/CuttingDownTrees.kt
+++ src/main/kotlin/com/lessons/fifth/CuttingDownTrees.kt
@@ -13,6 +13,25 @@ fun main() {
 
 fun determinateMinLengthSegment(treesVariety: List<Int>, numberOfVariety: Int) : Pair<Int, Int> {
 
+    // Массив флагов вхождения в диапазон
+    val flagsVariety = BooleanArray(treesVariety.size)
+
+    // Количество сортов на отрезке
+    var counterVariety = 0
+
+    // Определение количества сортов на отрезке
+    for (i in 0 until numberOfVariety) {
+
+        val currentVariety = treesVariety[i] - 1
+
+        // Нет сорта на отрезке
+        if (!flagsVariety[currentVariety]) {
+
+            flagsVariety[currentVariety] = true
+            counterVariety++
+        }
+    }
+
     var start = 0
     var finish = treesVariety.lastIndex
 
