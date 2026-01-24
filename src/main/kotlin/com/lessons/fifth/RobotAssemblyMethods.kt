939ddd44910af1be8d0a77e9c1fef1a593188212
kerikir <vlasov-kerik@mail.ru>
2026-01-24 22:27:09 +0300

    added calculation of the number of ways

--- src/main/kotlin/com/lessons/fifth/RobotAssemblyMethods.kt
+++ src/main/kotlin/com/lessons/fifth/RobotAssemblyMethods.kt
@@ -20,8 +20,21 @@ fun main() {
 fun definitionRobotAssemblyMethods(operations: String, memory: Int) : Int {
 
     var currentLength = 0
+    var numberOfWays = 0
 
-    
+    // Пробег окном
+    for (i in memory..operations.lastIndex) {
 
-    return currentLength
+        if (operations[i] == operations[i - memory]) {
+            // Экономически целесообразно использовать робота
+            currentLength++
+            numberOfWays += currentLength
+
+        } else {
+            // Операции записанные в памяти не повторяются
+            currentLength = 0
+        }
+    }
+
+    return numberOfWays
 }
