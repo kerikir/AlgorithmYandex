e4aa1655fb7bd858284c1775f718614ca09ebf0f
kerikir <vlasov-kerik@mail.ru>
2026-01-24 22:21:18 +0300

    added receiving input data

---
+++ src/main/kotlin/com/lessons/fifth/RobotAssemblyMethods.kt
@@ -0,0 +1,27 @@
+package com.lessons.fifth
+
+
+
+/*
+Время =
+Память =
+ */
+fun main() {
+
+    val k = readLine()!!.trim().toInt()
+    val operations = readLine()!!.trim()
+
+    val result = definitionRobotAssemblyMethods(operations, k)
+    println(result)
+}
+
+
+
+fun definitionRobotAssemblyMethods(operations: String, memory: Int) : Int {
+
+    var currentLength = 0
+
+    
+
+    return currentLength
+}
