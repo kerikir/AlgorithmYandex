7c077657b3b90267dd262a4348a5822cb18d04bb
kerikir <vlasov-kerik@mail.ru>
2026-01-25 22:12:55 +0300

    added counting of character occurrences

--- src/main/kotlin/com/lessons/fifth/MaxSubstring.kt
+++ src/main/kotlin/com/lessons/fifth/MaxSubstring.kt
@@ -19,5 +19,29 @@ fun main() {
 
 fun determinateMaxSubstring(string: String, maxRepeat: Int) : Pair<Int, Int> {
 
+    val repeatChar = mutableMapOf<Char, Int>()
+
+    var right = 0
+    // Двигаем левый указатель - начало подстроки
+    for (left in string.indices) {
+
+        while (right < string.length) {
+
+            // Количество повторений буквы в подстроке
+            val count = repeatChar.getOrDefault(string[right], 0)
+
+            if (count < maxRepeat) {
+                // Подстрока продолжается
+                repeatChar[string[right]] = count + 1
+
+            } else {
+                // Новый символ не подходит для подстроки
+                break
+            }
+
+            right++
+        }
+    }
+
     return 1 to 1
 }
