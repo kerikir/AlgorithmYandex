f36ef39d76b260d251f0fc66c6de797334c8c12d
kerikir <vlasov-kerik@mail.ru>
2026-01-25 22:50:59 +0300

    the pointer for the offset has been changed

--- src/main/kotlin/com/lessons/fifth/MaxSubstring.kt
+++ src/main/kotlin/com/lessons/fifth/MaxSubstring.kt
@@ -25,37 +25,31 @@ fun determinateMaxSubstring(string: String, maxRepeat: Int) : Pair<Int, Int> {
     var startIndexMaxLength = 0
     var currentLengthSubstring = 0
 
-    var right = 0
-    // Двигаем левый указатель - начало подстроки
-    for (left in string.indices) {
+    var left = 0
 
-        while (right < string.length) {
+    // Двигаем правый указатель - конец подстроки
+    for (right in string.indices) {
 
-            // Количество повторений буквы в подстроке
-            val count = repeatChar.getOrDefault(string[right], 0) + 1
-            repeatChar[string[right]] = count
+        // Количество повторений буквы в подстроке
+        val count = repeatChar.getOrDefault(string[right], 0) + 1
+        repeatChar[string[right]] = count
 
-            if (count <= maxRepeat) {
-                // Подстрока продолжается
-                currentLengthSubstring++
+        // Определяем максимальную подстроку для правого символа
+        while (repeatChar[string[right]]!! > maxRepeat) {
 
-                if (currentLengthSubstring > maxLengthSubstring) {
-                    // Нашли наибольшую подстроку
-                    maxLengthSubstring = currentLengthSubstring
-                    startIndexMaxLength = left + 1
-                }
-
-            } else {
-                // Новый символ не подходит для подстроки
-                break
-            }
-
-            right++
+            // Двигаем левый указатель - начало подстроки
+            repeatChar[string[left]] = repeatChar.getOrDefault(string[left], 1) - 1
+            left++
         }
 
-        // Символ ушел из подстроки
-        repeatChar[string[left]] = repeatChar.getOrDefault(string[left], 1) - 1
-        currentLengthSubstring--
+        // Определение длины подстроки
+        currentLengthSubstring = right - left + 1
+
+        if (currentLengthSubstring > maxLengthSubstring) {
+            // Нашли наибольшую подстроку
+            maxLengthSubstring = currentLengthSubstring
+            startIndexMaxLength = left + 1
+        }
     }
 
     return maxLengthSubstring to startIndexMaxLength
