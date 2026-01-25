8f70453517480259c75c50c4f8b573311e0f7275
kerikir <vlasov-kerik@mail.ru>
2026-01-25 23:16:35 +0300

    added the comments in the MaxSubstring.kt

--- src/main/kotlin/com/lessons/fifth/MaxSubstring.kt
+++ src/main/kotlin/com/lessons/fifth/MaxSubstring.kt
@@ -3,8 +3,10 @@ package com.lessons.fifth
 
 
 /*
-Время =
-Память =
+Время = 339ms
+Память = 18.64Mb
+
+Сложность = O(N + N) = O(N)
  */
 fun main() {
 
@@ -28,14 +30,14 @@ fun determinateMaxSubstring(string: String, maxRepeat: Int) : Pair<Int, Int> {
     var left = 0
 
     // Двигаем правый указатель - конец подстроки
-    for (right in string.indices) {
+    for (right in string.indices) {     // O(N)
 
         // Количество повторений буквы в подстроке
         val count = repeatChar.getOrDefault(string[right], 0) + 1
         repeatChar[string[right]] = count
 
         // Определяем максимальную подстроку для правого символа
-        while (repeatChar[string[right]]!! > maxRepeat) {
+        while (repeatChar[string[right]]!! > maxRepeat) {   // O(N)
 
             // Двигаем левый указатель - начало подстроки
             repeatChar[string[left]] = repeatChar.getOrDefault(string[left], 1) - 1
@@ -45,7 +47,7 @@ fun determinateMaxSubstring(string: String, maxRepeat: Int) : Pair<Int, Int> {
         // Определение длины подстроки
         currentLengthSubstring = right - left + 1
 
-        if (currentLengthSubstring > maxLengthSubstring) {
+        if (currentLengthSubstring > maxLengthSubstring) {  // O(1)
             // Нашли наибольшую подстроку
             maxLengthSubstring = currentLengthSubstring
             startIndexMaxLength = left + 1
