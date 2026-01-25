6299a319bce03ee9a6f72c43d9a00bf811a812d1
kerikir <vlasov-kerik@mail.ru>
2026-01-25 14:24:44 +0300

    added a definition of the number of each card

--- src/main/kotlin/com/lessons/fifth/GameScore.kt
+++ src/main/kotlin/com/lessons/fifth/GameScore.kt
@@ -22,5 +22,14 @@ fun determinateVariationsOfGameScore(cards: List<Int>, maxDiff: Int) : Long {
 
     var counter = 0L
 
+    val numberOfCards = mutableMapOf<Int, Int>()
+
+    // Определение количества каждой карточки
+    for (card in cards) {
+
+        val count = numberOfCards.getOrDefault(card, 0)
+        numberOfCards[card] = count + 1
+    }
+
     return counter
 }
