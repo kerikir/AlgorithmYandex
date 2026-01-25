a053035fa32d71dd3c105fe1c7e42e3750fdf7ff
kerikir <vlasov-kerik@mail.ru>
2026-01-25 16:16:47 +0300

    the overflow problem has been solved

--- src/main/kotlin/com/lessons/fifth/GameScore.kt
+++ src/main/kotlin/com/lessons/fifth/GameScore.kt
@@ -43,7 +43,7 @@ fun determinateVariationsOfGameScore(cards: List<Int>, maxDiff: Int) : Long {
         // Двигаем правый указатель - допустимая карточка для разницы в счете
         while (
             (right < scoreCards.size) &&
-            (scoreCards[right] <= (scoreCards[left] * maxDiff))
+            (scoreCards[right].toLong() <= (scoreCards[left] * maxDiff.toLong()))
         ) {
             // Считаем количество дубликатов каждой подходящей карточки
             if (numberOfCards[scoreCards[right]]!! >= 2) {
@@ -54,23 +54,23 @@ fun determinateVariationsOfGameScore(cards: List<Int>, maxDiff: Int) : Long {
         }
 
         // Ведущая карточка повторяется один раз с разными карточками
-        counter += 3 * (right - left - 2) * (right - left - 1)
+        counter += 3L * (right - left - 2) * (right - left - 1)
 
         // Ведущая карточка повторяется два раза с разными карточками
         if (numberOfCards[scoreCards[left]]!! >= 2) {
 
-            counter += (right - left - 1) * 3
+            counter += (right - left - 1) * 3L
             dublicated--
         }
 
         // Ведущая карточка повторяется три раза
         if (numberOfCards[scoreCards[left]]!! >= 3) {
 
-            counter++
+            counter += 1L
         }
 
         // Ведущая карточка повторяется один раз с дубликатоми каждой карточки
-        counter += 3 * dublicated
+        counter += 3L * dublicated
     }
 
     return counter
