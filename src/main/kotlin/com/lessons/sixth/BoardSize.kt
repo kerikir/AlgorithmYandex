b2969aac792d532749bab367b5cbc3cba4cab3fa
kerikir <vlasov-kerik@mail.ru>
2026-01-27 01:38:25 +0300

    implemented a left-hand binary search

--- src/main/kotlin/com/lessons/sixth/BoardSize.kt
+++ src/main/kotlin/com/lessons/sixth/BoardSize.kt
@@ -3,6 +3,11 @@ package com.lessons.sixth
 import kotlin.math.min
 
 
+
+/*
+Время =
+Память =
+ */
 fun main() {
 
     val (w, h, n) = readLine()!!.trim().split(' ').map { it.toInt() }
@@ -12,21 +17,42 @@ fun main() {
 }
 
 
+
 fun determinateMinBoardSize(numberOfDiplomas: Int, diplomaHeight: Int, diplomaWidth: Int) : Int {
 
     var maxBoardSize = numberOfDiplomas * min(diplomaHeight, diplomaWidth)
 
     val sizesOfBoard = IntArray(maxBoardSize) { it + 1 }
 
-    for (size in sizesOfBoard) {
+    val size = binarySearchBoardSize(sizesOfBoard, numberOfDiplomas, diplomaHeight, diplomaWidth)
 
-        val isCorrect = true
+    return size
+}
 
-        if (isCorrect) {
 
-            return size
+
+/**
+ * Левый бинарный поиск.
+ * Поиск наименьшего размера каждой стороны доски.
+ */
+fun binarySearchBoardSize(sizesOfBoard: IntArray, numberOfDiplomas: Int, diplomaHeight: Int, diplomaWidth: Int)
+: Int {
+
+    var leftBorder = 0
+    var rightBorder = sizesOfBoard.lastIndex
+
+    // Поиск минимальных размеров доски для всех дипломов
+    while (leftBorder < rightBorder) {
+
+        val middle = (rightBorder + leftBorder) / 2
+
+        if (true) {
+
+            rightBorder = middle
+        } else {
+            leftBorder = middle + 1
         }
     }
 
-    return maxBoardSize
+    return sizesOfBoard[leftBorder]
 }
