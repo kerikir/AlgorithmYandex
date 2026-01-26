6b5f1aec0d4bf945c48c3c6a987fcad71d3b8f7a
kerikir <vlasov-kerik@mail.ru>
2026-01-26 22:43:08 +0300

    implemented a left-hand binary search

--- src/main/kotlin/com/lessons/sixth/NearestNumber.kt
+++ src/main/kotlin/com/lessons/sixth/NearestNumber.kt
@@ -22,5 +22,38 @@ fun determinateNearestNumber(array: IntArray, listNumbers: List<Int>) : String {
 
     val resultList = mutableListOf<String>()
 
+    // Пробег по всем числам
+    for (number in listNumbers) {
+
+    }
+
     return resultList.joinToString("\n")
+}
+
+
+
+/**
+ * Левый бинарный поиск.
+ * Поиск наименьшего числа из больше либо равных `number`.
+ */
+fun binarySearchLargerNumber(array: IntArray, number: Int) : Int {
+
+    var leftBorder = 0
+    var rightBorder = array.lastIndex
+
+    while (leftBorder < rightBorder) {
+
+        val middle = (rightBorder + leftBorder) / 2
+
+        if (array[middle] >= number) {
+
+            rightBorder = middle
+
+        } else {
+
+            leftBorder = middle + 1
+        }
+    }
+
+    return array[leftBorder]
 }
