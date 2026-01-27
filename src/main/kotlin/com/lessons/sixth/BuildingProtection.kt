8c4a488d739b5a3e4c400bb963e070a1c82daef0
kerikir <vlasov-kerik@mail.ru>
2026-01-27 03:58:50 +0300

    added a check for correct building protection sizes

--- src/main/kotlin/com/lessons/sixth/BuildingProtection.kt
+++ src/main/kotlin/com/lessons/sixth/BuildingProtection.kt
@@ -33,11 +33,42 @@ fun determinateMaxSizeBuildingProtection(
 
         val middle = (leftBorder + rightBorder + 1) / 2
 
-        if (true) {
+        val isCorrect = checkCorrectSizeBuildingProtection(middle, numberOfModules,
+            moduleWidth, moduleHeight, fieldWidth, fieldHeight)
+
+        if (isCorrect) {
             leftBorder = middle
 
         } else {
             rightBorder = middle - 1
         }
     }
+
+    return leftBorder
+}
+
+
+
+fun checkCorrectSizeBuildingProtection(
+    sizeBuildingProtection: Long, numberOfModules: Long, moduleWidth: Long,
+    moduleHeight: Long, fieldWidth: Long, fieldHeight: Long
+) : Boolean {
+
+    var isCorrect = false
+
+    // Размеры каждого модуля по ширине и высоте с защитой
+    val sizeModuleByWidth = moduleWidth + 2 * sizeBuildingProtection
+    val sizeModuleByHeight = moduleHeight + 2 * sizeBuildingProtection
+
+    // Количество модулей по ширине и высоте на поле
+    val numberOfModulesByWidth = fieldWidth / sizeModuleByWidth
+    val numberOfModulesByHeight = fieldHeight / sizeModuleByHeight
+
+    // Можно ли разместить все модули с текущей защитой
+    if ((numberOfModulesByHeight * numberOfModulesByWidth) >= numberOfModules) {
+
+        isCorrect = true
+    }
+
+    return isCorrect
 }
