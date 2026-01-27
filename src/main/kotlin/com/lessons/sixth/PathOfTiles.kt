0fe781f1174321045216d620ac1f4bfa3ae8e7fe
kerikir <vlasov-kerik@mail.ru>
2026-01-28 02:05:08 +0300

    added a check for correct width path of tiles

--- src/main/kotlin/com/lessons/sixth/PathOfTiles.kt
+++ src/main/kotlin/com/lessons/sixth/PathOfTiles.kt
@@ -9,16 +9,17 @@ import kotlin.math.min
  */
 fun main() {
 
-    val n = readLine()!!.trim().toInt()
-    val m = readLine()!!.trim().toInt()
+    val n = readLine()!!.trim().toLong()
+    val m = readLine()!!.trim().toLong()
     val t = readLine()!!.trim().toLong()
 
-
+    val result = determinateWidthPathOfTiles(n, m, t)
+    println(result)
 }
 
 
 
-fun determinateWidthPathOfTiles(n: Int, m: Int, numberOfTiles: Long) : Int {
+fun determinateWidthPathOfTiles(n: Long, m: Long, numberOfTiles: Long) : Long {
 
     var leftBorder = 0L
     // Максимальная ширина дорожки - отсутствие клумбы по центру
@@ -28,7 +29,7 @@ fun determinateWidthPathOfTiles(n: Int, m: Int, numberOfTiles: Long) : Int {
 
         val middle = (rightBorder + leftBorder + 1L) / 2L
 
-        val isCorrect = true
+        val isCorrect = checkCorrectWidthPathOfTiles(middle, n, m, numberOfTiles)
 
         if (isCorrect) {
             leftBorder = middle
@@ -37,4 +38,26 @@ fun determinateWidthPathOfTiles(n: Int, m: Int, numberOfTiles: Long) : Int {
             rightBorder = middle - 1L
         }
     }
+
+    return leftBorder
+}
+
+
+
+fun checkCorrectWidthPathOfTiles(width: Long, n: Long, m: Long, numberOfTiles: Long) : Boolean {
+
+    var counter = 1L
+    var numberOfTilesSaved = 1L
+
+    // Расчет сэкономленых плит
+    while (counter < width) {
+
+        numberOfTilesSaved += counter
+        counter++
+    }
+
+    // Потраченные плитки на определенную ширину для дорожки
+    val numberOfTilesSpent = width * (2L * n + 2L * m - 4L) - numberOfTilesSaved
+
+    return numberOfTiles >= numberOfTilesSpent
 }
