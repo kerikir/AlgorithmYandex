2d63f8c8aeb3aa57a55be8778bc5a6c9068aae9e
kerikir <vlasov-kerik@mail.ru>
2026-01-29 19:07:03 +0300

    implemented a left-hand binary search

--- src/main/kotlin/com/lessons/sixth/MedianOfSequences.kt
+++ src/main/kotlin/com/lessons/sixth/MedianOfSequences.kt
@@ -1,6 +1,14 @@
 package com.lessons.sixth
 
+import kotlin.math.max
+import kotlin.math.min
 
+
+
+/*
+Время =
+Память =
+ */
 fun main() {
 
     val (n, l) = readLine()!!.trim().split(' ').map { it.toInt() }
@@ -10,4 +18,34 @@ fun main() {
         val sequence = readLine()!!.trim().split(' ').map { it.toInt() }.toIntArray()
         sequences.add(sequence)
     }
+
+    // Перебор всех пар последовательностей
+    for (i in 0 until sequences.lastIndex - 1) {
+        for (j in (i + 1)..sequences.lastIndex) {
+            println(determinateLeftMedianOfSequences(sequences[i], sequences[j], l))
+        }
+    }
+}
+
+
+
+fun determinateLeftMedianOfSequences(sequenceFirst: IntArray, sequenceSecond: IntArray, median: Int) : Int {
+
+    // Определение границ значения медианы
+    var leftBorder = min(sequenceFirst[0], sequenceSecond[0])
+    var rightBorder = max(sequenceFirst[sequenceFirst.lastIndex], sequenceSecond[sequenceSecond.lastIndex])
+
+    while (leftBorder < rightBorder) {
+
+        val middle = (leftBorder + rightBorder) / 2
+
+        if (true) {
+            rightBorder = middle
+
+        } else {
+            leftBorder = middle + 1
+        }
+    }
+
+    return leftBorder
 }
