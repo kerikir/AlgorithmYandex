8a54d4230a11e47778d8a77b3070752f411987ac
kerikir <vlasov-kerik@mail.ru>
2026-09-04 17:05:39 +0300

    added sorting of input data

--- src/main/kotlin/com/lessons/seventh/SegmentsAndPoints.kt
+++ src/main/kotlin/com/lessons/seventh/SegmentsAndPoints.kt
@@ -9,11 +9,19 @@ package com.lessons.seventh
 fun main() {
     val (n, m) = readln().trim().split(Regex("\\s+")).map { it.toInt() }
     val segments = ArrayList<Pair<Int, Int>>(n)
-    for (i in segments.indices) {
+    for (i in 0..<n) {
         val (a, b) = readln().trim().split(Regex("\\s+")).map { it.toInt() }
-        segments[i] = a to b
+        segments.add(a to b)
     }
     val points = readln().trim().split(Regex("\\s+")).map { it.toInt() }
 
-    
+    val segmentsSorted = segments.sortedWith(
+        compareBy<Pair<Int, Int>> { it.first }
+            .thenBy { it.second }
+    )
+    val pointsSorted = points.sorted()
+
+
+    val result = IntArray(m)
+    println(result.joinToString(" "))
 }
