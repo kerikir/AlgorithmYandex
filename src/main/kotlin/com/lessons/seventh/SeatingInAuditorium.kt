8bba4c35ff95b3b48e0ab2234a199254bd0b870b
kerikir <vlasov-kerik@mail.ru>
2026-09-05 14:58:33 +0300

    added a solution to the problem of distributing exam options

---
+++ src/main/kotlin/com/lessons/seventh/SeatingInAuditorium.kt
@@ -0,0 +1,36 @@
+package com.lessons.seventh
+
+fun main() {
+    val (n, d) = readLine()!!.trim().split(Regex("\\s+")).map { it.toInt() }
+    val coords = readLine()!!.trim().split(Regex("\\s+")).map { it.toInt() }
+
+    val coordsSorted = coords.sorted()
+
+    var maxLength = 1
+    var right = 1
+    for (left in coords.indices) {
+        var length = right - left
+
+        while (right < n && coordsSorted[right] - coordsSorted[left] <= d) {
+            right++
+            length++
+        }
+
+        if (length > maxLength) {
+            maxLength = length
+        }
+    }
+
+    val result = mutableMapOf<Int, Int>()
+    coordsSorted.forEachIndexed { index, item ->
+        result[item] = index % maxLength + 1
+    }
+
+    val sb = StringBuilder()
+    sb.append(maxLength).append('\n')
+    coords.forEachIndexed { index, item ->
+        if (index > 0) sb.append(' ')
+        sb.append(result[item])
+    }
+    println(sb.toString())
+}
